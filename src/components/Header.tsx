98d3263f35adbf974c19b4ebfd61538f18e36e88
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-09-04 22:50:52 +0300

    apply locale to header and footer.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router";
-import LangToggle from "./LangToggle";
+import LangToggle from "@/components/LangToggle";
 
 const Header = () => (
   <header className="w-full h-16 shrink-0 flex items-center justify-center bg-background/20 sticky top-0 z-20 backdrop-blur-xs border-b-background">
